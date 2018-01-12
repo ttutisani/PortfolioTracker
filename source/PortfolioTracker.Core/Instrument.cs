2cf8e332407894ace98bb033b513b4e7eafeb6c4
Tengiz <tengo_tutisani@hotmail.com>
2018-01-11 21:16:46 -0600

    Instrument class.

---
+++ source/PortfolioTracker.Core/Instrument.cs
@@ -0,0 +1,18 @@
+﻿namespace PortfolioTracker.Core
+{
+    public sealed class Instrument
+    {
+        public Instrument(string symbol, string name, decimal currentPrice)
+        {
+            Symbol = symbol ?? throw new System.ArgumentNullException(nameof(symbol));
+            Name = name ?? throw new System.ArgumentNullException(nameof(name));
+            CurrentPrice = currentPrice;
+        }
+
+        public string Symbol { get; private set; }
+
+        public string Name { get; private set; }
+
+        public decimal CurrentPrice { get; private set; }
+    }
+}
---
+++ source/PortfolioTracker.UnitTests/InstrumentTests.cs
@@ -0,0 +1,31 @@
+﻿using PortfolioTracker.Core;
+using System;
+using Xunit;
+
+public sealed class InstrumentTests
+{
+    [Fact]
+    public void Constructor_Requires_All_Arguments()
+    {
+        //act / assert.
+        Assert.Throws<ArgumentNullException>(() => new Instrument(null, "name 123", 123m));
+        Assert.Throws<ArgumentNullException>(() => new Instrument("SMB", null, 123m));
+    }
+
+    [Fact]
+    public void Constructor_Initializes_All_Properties()
+    {
+        //arrange.
+        var expectedSymbol = "SMB";
+        var expectedName = "name 123";
+        var expectedPrice = 112121m;
+
+        //act.
+        var sut = new Instrument(expectedSymbol, expectedName, expectedPrice);
+
+        //assert.
+        Assert.Equal(expectedSymbol, sut.Symbol);
+        Assert.Equal(expectedName, sut.Name);
+        Assert.Equal(expectedPrice, sut.CurrentPrice);
+    }
+}
